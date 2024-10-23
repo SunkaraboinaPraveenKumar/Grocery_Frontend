79ecb48ea49349699e095b9f2b4d04fd3e9ab477
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-10-23 09:31:12 +0530

    url chnages

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const axiosClient= axios.create({
-    baseURL:`http://192.168.10.141:1337/api`
+    baseURL:`http://localhost:1337/api`
 })
 
 const getCategory=()=>axiosClient.get("/categories?populate=*");
